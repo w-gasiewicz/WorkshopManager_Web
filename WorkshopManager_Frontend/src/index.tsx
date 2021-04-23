8aa4299a8b44ed940c0fd857b8071e1dcea8353d
Wojciech Gąsiewicz <235086@student.pwr.edu.pl>
2021-04-23 19:34:25 +0200

    login form added

--- WorkshopManager_Frontend/src/index.tsx
+++ WorkshopManager_Frontend/src/index.tsx
@@ -1,15 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
+import { BrowserRouter } from 'react-router-dom';
+import './styles/index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+const baseUrl = document.getElementsByTagName('base')[0];
+const rootElement = document.getElementById('root');
+
 ReactDOM.render(
-  <React.StrictMode>
+  <BrowserRouter basename={String(baseUrl)} >
     <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+  </BrowserRouter>,
+  rootElement);
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
