cd827f65bb4a7cfdd03fc46e9cc60099a3372122
Wojciech Gąsiewicz <235086@student.pwr.edu.pl>
2021-04-26 18:25:02 +0200

    particles added

--- WorkshopManager_Frontend/src/App.jsx
+++ WorkshopManager_Frontend/src/App.jsx
@@ -2,28 +2,29 @@ import { Route } from 'react-router';
 import { Login } from './views/Login';
 import './styles/App.css';
 import { Contact } from './views/Contact';
+import Particles from 'react-particles-js';
+import ParticlesConfig from './configs/ParticlesConfig';
+import logo from './logo.svg';
 
 function App() {
+  //return (
+  //   <div style={{ position: 'relative', overflow: "hidden" }}>
+  //     <div style={{ position: 'absolute' }}>
+  //       <Particles height="100vh" width="100vw" params={ParticlesConfig} />
+  //     </div>
+  //     <Route exact path='/' component={Login} />
+  //     <Route exact path='/contact' component={Contact} />
+  //   </div>
+  // );
   return (
-    // <div className="App">
-    //   {/* <header className="App-header">
-    //     <img src={logo} className="App-logo" alt="logo" />
-    //     <p>
-    //       Edit <code>src/App.tsx</code> and save to reload.
-    //     </p>
-    //     <a
-    //       className="App-link"
-    //       href="https://reactjs.org"
-    //       target="_blank"
-    //       rel="noopener noreferrer"
-    //     >
-    //       Learn React
-    //     </a>
-    //   </header> */}
-    // </div>
     <div>
-    <Route exact path='/' component={Login} />
-    <Route exact path='/contact' component={Contact} />
+      <div style={{ position: 'absolute' }}>
+        <Particles params={ParticlesConfig} />
+      </div>
+      <header className="App-header">
+        <Route exact path='/' component={Login} />
+        <Route exact path='/contact' component={Contact} />
+      </header>
     </div>
   );
 }
